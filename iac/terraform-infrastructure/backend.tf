41bba77837aee04345b347305631a9285750fcf2
Fred B <fredbitenyo@gmail.com>
2023-09-22 22:03:14 +0200

    testing workflows with the new platform

--- iac/terraform-infrastructure/backend.tf
+++ iac/terraform-infrastructure/backend.tf
@@ -1,7 +1,7 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = "s3-terraform-for-k8s-fibonacci"
+    bucket         = "s3-terraform-for-k8s-fibo"
     key            = "terraform-modules/fibonacci/terraform.tfstate"
     region         = "us-east-1"
     profile        = "devops"
