40b3b66f7f6282fee9a13ccf5b5286ee49f58b82
Fred B <fredbitenyo@gmail.com>
2023-09-28 19:21:17 +0200

    testing eksctl

--- cicd/setup-eks.sh
+++ cicd/setup-eks.sh
@@ -2,6 +2,10 @@
 
 # just or the sake of testing
 
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
 echo "let's create an eks cluster"
 eksctl create cluster --name firstcluster --region us-east-1 --nodegroup-name eksgroup --node-type t2.small --nodes 2
 echo "done"
