02a2f23ca32fa18170cbff085c26411beca3dd77
Fred B <fredbitenyo@gmail.com>
2023-09-29 08:13:38 +0200

    debugging eksctl

--- cicd/setup-eks.sh
+++ cicd/setup-eks.sh
@@ -9,4 +9,4 @@ eksctl version
 echo "let's create an eks cluster"
 eksctl create cluster --name firstcluster --region us-east-1 --nodegroup-name eksgroup --node-type t2.small --nodes 2
 echo "done"
-aws eks update-kubeconfig --name firstcluster --region us-east-1
+# aws eks update-kubeconfig --name firstcluster --region us-east-1
