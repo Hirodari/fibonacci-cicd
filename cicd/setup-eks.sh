62e8618e49bfaa045cb04718657b51b65c9b5448
Fred B <fredbitenyo@gmail.com>
2023-09-29 08:52:11 +0200

    testing creating cluster with codebuild

--- cicd/setup-eks.sh
+++ cicd/setup-eks.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# just or the sake of testing
-
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/local/bin
-eksctl version
-
 echo "let's create an eks cluster"
-# eksctl create cluster --name firstcluster --region us-east-1 --nodegroup-name eksgroup --node-type t2.small --nodes 2
-eksctl delete cluster --name firstcluster
+eksctl create cluster --name firstcluster --region us-east-1 --nodegroup-name eksgroup --node-type t2.small --nodes 2
+# eksctl delete cluster --name firstcluster
 echo "done"
 # aws eks update-kubeconfig --name firstcluster --region us-east-1
