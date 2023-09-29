53bd76d9c9db777381a6844e7231e2b01edfc445
Fred B <fredbitenyo@gmail.com>
2023-09-29 08:27:56 +0200

    debugging eksctl

--- cicd/setup-eks.sh
+++ cicd/setup-eks.sh
@@ -7,6 +7,7 @@ sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
 echo "let's create an eks cluster"
-eksctl create cluster --name firstcluster --region us-east-1 --nodegroup-name eksgroup --node-type t2.small --nodes 2
+# eksctl create cluster --name firstcluster --region us-east-1 --nodegroup-name eksgroup --node-type t2.small --nodes 2
+eksctl delete cluster --name firstcluster
 echo "done"
 # aws eks update-kubeconfig --name firstcluster --region us-east-1
