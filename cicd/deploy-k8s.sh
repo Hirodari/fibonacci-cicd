15a72bae5b48c7c718e98aecb1107f8debd4a545
Fred B <fredbitenyo@gmail.com>
2023-09-30 13:36:01 +0200

    testing eks deployment

--- cicd/deploy-k8s.sh
+++ cicd/deploy-k8s.sh
@@ -4,8 +4,10 @@ echo "connecting to the correct cluster"
 aws eks update-kubeconfig --name firstcluster --region us-east-1
 kubectl get nodes
 
+echo "ingress nginx and cert-manager completed"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/aws/deploy
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.0/cert-manager.yaml
+
 kubectl get services
 
-kubectl apply -f k8s/
+kubectl apply -f ../k8s/
