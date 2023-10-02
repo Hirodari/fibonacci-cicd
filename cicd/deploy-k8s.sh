ffa71897e33b63ac160ab2e020a160b728552e49
Fred B <fredbitenyo@gmail.com>
2023-10-02 23:14:23 +0200

    deleting code deploy

--- cicd/deploy-k8s.sh
+++ cicd/deploy-k8s.sh
@@ -8,18 +8,18 @@ echo "deploy terraform"
 pwd
 cd ../iac/terraform-infrastructure
 terraform init
-terraform apply --auto-approve
+terraform destroy --auto-approve
 
-echo "connecting to the correct cluster"
-aws eks update-kubeconfig --name firstcluster --region us-east-1
-kubectl get nodes
+# echo "connecting to the correct cluster"
+# aws eks update-kubeconfig --name firstcluster --region us-east-1
+# kubectl get nodes
 
-kubectl apply -f ../../k8s/
+# kubectl apply -f ../../k8s/
 
-echo "ingress nginx and cert-manager completed"
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/aws/deploy.yaml
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.0/cert-manager.yaml
+# echo "ingress nginx and cert-manager completed"
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/aws/deploy.yaml
+# kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.0/cert-manager.yaml
 
-exit 0
-kubectl get services
+# exit 0
+# kubectl get services
 
