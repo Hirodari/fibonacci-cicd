cc2283da45d8b155ba1026d64e01400b4dc466ca
Fred B <fredbitenyo@gmail.com>
2023-10-02 23:01:54 +0200

    launching code deploy

--- cicd/deploy-k8s.sh
+++ cicd/deploy-k8s.sh
@@ -14,11 +14,12 @@ echo "connecting to the correct cluster"
 aws eks update-kubeconfig --name firstcluster --region us-east-1
 kubectl get nodes
 
-kubectl apply -f ../../k8s/api-deployment.yml
+kubectl apply -f ../../k8s/
 
 echo "ingress nginx and cert-manager completed"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/aws/deploy.yaml
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.0/cert-manager.yaml
 
+showStatus
 kubectl get services
 
